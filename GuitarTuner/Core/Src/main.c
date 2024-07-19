60722fc1a311e0471cc110bc653ad0f04fe08796
frmennuti <frmennuti@gmail.com>
2024-07-19 13:42:02 +0200

    Buffer and RTOS
    change buffer size to exploit half conversion callback from HAL.
    Set up Free RTOS to be the basis of the framework that will implement active objects

--- GuitarTuner/Core/Src/main.c
+++ GuitarTuner/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -31,7 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define ADC_BUFFER_LENGTH 1024
+#define ADC_BUFFER_LENGTH 2048
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -48,6 +49,13 @@ TIM_HandleTypeDef htim7;
 
 UART_HandleTypeDef huart2;
 
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
 /* USER CODE BEGIN PV */
 uint32_t buffer_adc[ADC_BUFFER_LENGTH];
 /* USER CODE END PV */
@@ -60,6 +68,8 @@ static void MX_USART2_UART_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM6_Init(void);
 static void MX_TIM7_Init(void);
+void StartDefaultTask(void *argument);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -108,6 +118,42 @@ int main(void)
   HAL_TIM_Base_Start(&htim6);
   HAL_TIM_Base_Start(&htim7);
   /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
@@ -357,7 +403,7 @@ static void MX_DMA_Init(void)
 
   /* DMA interrupt init */
   /* DMA1_Channel1_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 
 }
@@ -409,6 +455,45 @@ void  HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 	HAL_GPIO_TogglePin(GPIOA, Trace0_Pin);
 }
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
--- README.md
+++ README.md
@@ -17,9 +17,15 @@ Fourier Transform:
 - fft resolution should be of at least 3Hz (more or less 10 bins between the 2 tones nearer to each other: E2 and A2)
 - user needs to be notified with a new value of the tone played at least each 200ms
 
-The solution is to set up the microcontroller to sample with a sampling frequency of 5120 Hz, and use DMA buffer of 1024 elements. In this way the buffer will be filled each 200ms, meaning that the interrupt of buffer full will be called each 200ms.
+The solution is to set up the microcontroller to sample with a sampling frequency of 5120 Hz, and use DMA buffer of 2048 elements. In this way the buffer will be filled each 400ms, meaning that the interrupt of buffer full will be called each 400ms.
 
-The number of elements in the DMA buffer is not enough to satisfy the frequency resolution stated above, therefore a second rolling buffer will be instantiated and handled with FIFO logic so that the fft will be performed on a rolling buffer of 2048 elements meeting all the requirements.
-The fft can be performed on a rolling buffer of 2048 elements (improve efficiency) each time 1024 elements are acquired and after having discarded the older 1024 elements.
+The system shall be able to communicate (which is the best solution? UART? USB? TBD) to the user the evaluated tone more than 3 times per second so that the user is constantly prompted the most recent played tone. Evaluating the fft each 400ms is not aligned with such requirement, therefore the half complete callback is exploited. Such callback is called each 200ms. Each half complete callback and complete callback a fft shall be triggered and result shall be communicated to the user.
 
-Performing fft on a 2048 elements array sampled with fs=5120Hz will provide us 1024 bins up to the fs/2=2560Hz component, therefore each bin will have resolution 2560/1024=2,5Hz
+Each time 1024 samples gets acquired (alternatively halfcomplete and complete callbacks) the first and last 1024 samples will be copied to the rolling 2048 buffer over which the fft will be evaluated.
+
+This will ensure responsiveness requirement and the resolution requirement since performing fft on a 2048 elements array sampled with fs=5120Hz will provide us 1024 bins up to the fs/2=2560Hz component, therefore each bin will have resolution 2560/1024=2,5Hz
+
+SW Architecture:
+The architecture is based upon Real Time Active Object implementation (https://github.com/QuantumLeaps/FreeACT?tab=readme-ov-file#directories-and-files).
+The framework is based upon the FreeRTOS real time operating system.
+
