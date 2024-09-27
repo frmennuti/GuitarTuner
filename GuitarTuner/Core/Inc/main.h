4ae9331eb3d377d10fea2006b3c54d5c43e6b74d
frmennuti <frmennuti@gmail.com>
2024-09-27 18:03:44 +0200

    Debug Session
    Added another GPIO (TraceIdle) to debug how much time is spent within the idle task. This could be my start point for further optimizations

--- GuitarTuner/Core/Inc/main.h
+++ GuitarTuner/Core/Inc/main.h
@@ -75,6 +75,8 @@ void Error_Handler(void);
 #define TCK_GPIO_Port GPIOA
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
+#define TraceIdle_Pin GPIO_PIN_6
+#define TraceIdle_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
